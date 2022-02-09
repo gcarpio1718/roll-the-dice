2dbc3a8e04ea467598e0ea66c14cfccdb74a0261
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-09 14:30:15 -0500

    change dice

--- six-sided.js
+++ six-sided.js
@@ -1,6 +1,4 @@
 const prompt = require('prompt-sync')();
 
 let diceRoll=Math.floor(Math.random()*6 + 1);
-    console.log("Roll dice: " + diceRoll);
-
-    
+    console.log("Dice roll output: " + diceRoll);
